1d3c80ef18b4f1d614e57e43a227048eb6254c02
Tõnis Ojandu <tonis.ojandu@reach-u.com>
2018-01-03 19:07:53 +0200

    more

--- TextProcessing.py
+++ TextProcessing.py
@@ -1,6 +1,7 @@
 import nltk
 import numpy
 import collections
+import time
 from os import listdir
 from nltk.stem import WordNetLemmatizer
 
@@ -40,7 +41,16 @@ for wikidata_file in wikidata_files:
                 bag_of_words[word] = 1
             else:
                 bag_of_words[word] = count + 1
-        bags.append(bag_of_words)
+
+        new_bag_of_words = dict()
+        for word, count in bag_of_words.items():
+            #print(word + " " + str(bag_of_words[word]))
+            new_bag_of_words[word] = count / len(tokens)
+            #print(word + " " + str(bag_of_words[word]))
+            #time.sleep(1)
+
+
+        bags.append(new_bag_of_words)
         # print(sorted(bag_of_words.items(), key=lambda pair: pair[1], reverse=True))
 
 # sets = list()
@@ -56,30 +66,78 @@ for wikidata_file in wikidata_files:
 # print(all_words)
 
 combined_bag = dict()
+occurences = dict()
 for bag in bags:
     for word, count in bag.items():
         combined_count = combined_bag.get(word)
         if combined_count is None:
             combined_bag[word] = count
+            occurences[word] = 1
         else:
             combined_bag[word] = combined_count + count
+            occurences[word] = occurences[word] + 1
 
 print(sum(count > 1 for count in combined_bag.values()))
-print(sorted(combined_bag.items(), key=lambda pair: pair[1], reverse=True))
+print(sorted(combined_bag.items(), key=lambda pair: pair[1], reverse=False))
 
 
-occurence_in_doc = dict()
+#occurence_in_doc = dict()
+#for word in combined_bag:
+#    word_list.append(word)
+#    count = 0
+#    for bag in bags:
+#        if word in bag:
+#            count = count + 1
+#    occurence_in_doc[word] = count
+
+
+word_list = []
 for word in combined_bag:
-    count = 0
-    for bag in bags:
+    if occurences[word] > 1:
+        word_list.append(word)
+
+#word_ranks = []
+#for word in word_list:
+#    ranks = []
+#    for bag in bags:
+#        if word in bag:
+#            ranks.append(bag[word])
+#        else:
+#            ranks.append(0)
+#    word_ranks.append(ranks)
+
+
+word_ranks = []
+for bag in bags:
+    ranks = []
+    for word in word_list:
         if word in bag:
-            count = count + 1
-    occurence_in_doc[word] = count
+            ranks.append(bag[word])
+        else:
+            ranks.append(0)
+    word_ranks.append(ranks)
 
+
+
+stds = numpy.std(numpy.array(word_ranks), axis=0)
+print(stds)
+
+word_std = dict()
 
 print()
 print()
-print("Occurences")
-print(occurence_in_doc)
+for i in range(0, len(word_list)):
+    word_std[word_list[i]] = stds[i]
+    #print(word_list[i] + " : " + str(stds[i]))
+
+
+
+print(sorted(word_std.items(), key=lambda pair: pair[1], reverse=False))
+
+allowed_words = set()
+#for word, std in word_std.items():
+#    if std
+
+
 
 
